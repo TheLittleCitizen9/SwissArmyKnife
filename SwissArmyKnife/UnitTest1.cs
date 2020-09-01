5cb7e45327cf9914ce1dc405552a6088e5a18928
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-01 20:55:53 +0300

    Finished third exercise

--- SwissArmyKnife/UnitTest1.cs
+++ SwissArmyKnife/UnitTest1.cs
@@ -1,7 +1,11 @@
+using Moq;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System;
+using System.Collections.Generic;
 using TestMe;
+using Telerik.JustMock;
+using Telerik.JustMock.Helpers;
 
 namespace SwissArmyKnife
 {
@@ -32,6 +36,25 @@ namespace SwissArmyKnife
             var swiisArmyKnife = new TestMe.SwissArmyKnife();
 
             Assert.Throws<ArgumentException>(() => swiisArmyKnife.ParseStringToInt(number));
+        }
+
+        [Test]
+        public void Test3_PickFreshestFruit_Exception()
+        {
+            FruitProvider fruitProvider = new FruitProvider();
+            var swiisArmyKnife = new TestMe.SwissArmyKnife();
+            var mock = new Mock<TestMe.ILogger>();
+            mock.Setup(x => x.Log(It.IsAny<string>()));
+
+            try
+            {
+                swiisArmyKnife.PickFreshestFruit(fruitProvider, mock.Object);
+            }
+            catch(Exception e)
+            {
+
+            }
+            mock.Verify(x => x.Log(It.IsAny<string>()), Times.Once());
         }
     }
 }
